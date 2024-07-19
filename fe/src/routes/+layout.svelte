1230babe2827048253cab4bc54e738aca42f7a47
BigZhenjie <1741zhenjie@gmail.com>
2024-07-19 16:46:10 -0700

    sets up frontend slider

---
+++ fe/src/routes/+layout.svelte
@@ -0,0 +1,8 @@
+
+
+<div class="app">
+	<main>
+		<slot></slot>
+	</main>
+</div>
+<slot></slot><script>import "../app.css";</script>
---
+++ fe/src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import Slider from '$lib/components/ui/slider/slider.svelte';
+	let sliderValue = [0];
+</script>
+
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<section class="flex flex-col justify-center items-center h-screen w-full gap-10">
+	<h1 class="text-3xl">{sliderValue}</h1>
+	<Slider bind:value={sliderValue} max={100} step={1} class="w-[240px]"  />
+</section>
+
